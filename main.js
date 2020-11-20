dd2522e93b288ad937daa3cd01c630d277684eda
hemerson.silva <hemerson.silva@izap.com.br>
2020-11-20 12:15:15 -0300

    basic webview finished

---
+++ main.js
@@ -0,0 +1,41 @@
+const { app, BrowserWindow, globalShortcut } = require('electron')
+const config = require('./config')
+
+let win;
+
+function createWindow () {
+  win = new BrowserWindow({
+    width: 800,
+    altura: 600,
+    titleBarStyle: 'hidden',
+    alwaysOnTop: true,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  })
+
+  win.loadURL(config.url)
+  // win.webContents.openDevTools()
+}
+
+function toggleDevTools() {
+  win.webContents.toggleDevTools()
+}
+
+function createShortcuts() {
+  globalShortcut.register('CmdOrCtrl+J', toggleDevTools)
+}
+
+app.whenReady().then(createWindow).then(createShortcuts)
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow()
+  }
+})
